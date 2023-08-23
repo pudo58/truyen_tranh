bb9c4f07eadab592b60e78f7d90b58c1e67e49b4
tho.la <thobaikhe@gmail.com>
2023-08-23 16:15:04 +0700

    update ui

---
+++ web/src/main/java/vn/tholv/web/core/base/constant/ChapterImageConst.java
@@ -0,0 +1,6 @@
+package vn.tholv.web.core.base.constant;
+
+public class ChapterImageConst {
+    public static final String TYPE_IMAGE = "image";
+    public static final String TYPE_TEXT = "text";
+}
